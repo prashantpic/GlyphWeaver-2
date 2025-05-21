621698cbaf95c8b2d77fb0f5c50b05164e285168
prashantpic <prashantpic001@gmail.com>
2025-05-21 20:54:39 +0530

    Delete  glyph-puzzle-cloud-infra directory

---  glyph-puzzle-cloud-infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket-name" # To be configured via -backend-config or wrapper script
-    key            = "glyph-puzzle-cloud-infra/terraform.tfstate"
-    region         = "your-aws-region-for-tf-state" # To be configured via -backend-config or wrapper script
-    dynamodb_table = "glyph-puzzle-terraform-locks" # To be configured via -backend-config or wrapper script
-    encrypt        = true
-  }
-}
