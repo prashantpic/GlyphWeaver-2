eff128d0175be4be8634f0f1b23a055e90e5f4fe
prashantpic <prashantpic001@gmail.com>
2025-05-21 20:55:13 +0530

    Delete  glyph-puzzle-security directory

---  glyph-puzzle-security/src/common/constants/audit.constants.ts
+++
@@ -1,5 +0,0 @@
-export const AUDIT_OUTCOME_SUCCESS = 'SUCCESS';
-export const AUDIT_OUTCOME_FAILURE = 'FAILURE';
-export const AUDIT_OUTCOME_ATTEMPT = 'ATTEMPT';
-
-// Add other audit-related constants as needed
---  glyph-puzzle-security/src/secrets/interfaces/secret-provider.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface ISecretProvider {
-    /**
-     * Retrieves a secret value associated with the given key.
-     * @param key The key identifying the secret.
-     * @returns A promise that resolves to the secret string if found, or undefined otherwise.
-     */
-    getSecret(key: string): Promise<string | undefined>;
-}
